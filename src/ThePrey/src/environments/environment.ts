59963900a9976992eb6ff4fa915bed043d39938a
nikneem <ekeilholz+git@outlook.com>
2026-06-08 14:34:04 +0200

    Add Redis module for Azure deployment and update environment API URL

--- src/ThePrey/src/environments/environment.ts
+++ src/ThePrey/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000',
+  apiUrl:
+    'https://gateway.jollyfield-ab1afcde.westeurope.azurecontainerapps.io',
   auth0: {
     domain: 'theprey.eu.auth0.com',
     clientId: 'tJrm2nPrAX4kES7XEnjUsL38cqbAbraJ',
