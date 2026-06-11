903260a18db4ea797719c6b21780d942c10590f0
nikneem <ekeilholz+git@outlook.com>
2026-06-11 16:00:35 +0200

    feat: add DelayAnchorLatitude, DelayAnchorLongitude, and DelayPenaltyApplied columns to GameParticipants

---
+++ src/Games/HexMaster.ThePrey.Games.Data.Postgres/Migrations/20260611135315_AddHunterDelayAnchor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HexMaster.ThePrey.Games.Data.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHunterDelayAnchor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "DelayAnchorLatitude",
+                table: "GameParticipants",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "DelayAnchorLongitude",
+                table: "GameParticipants",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DelayPenaltyApplied",
+                table: "GameParticipants",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DelayAnchorLatitude",
+                table: "GameParticipants");
+
+            migrationBuilder.DropColumn(
+                name: "DelayAnchorLongitude",
+                table: "GameParticipants");
+
+            migrationBuilder.DropColumn(
+                name: "DelayPenaltyApplied",
+                table: "GameParticipants");
+        }
+    }
+}
